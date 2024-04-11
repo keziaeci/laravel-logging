82abb1c79706db4e701f051fcc1f48eb6479c962
keziaeci <keziaeci2806@gmail.com>
2024-04-11 09:27:19 +0700

    add formatter

--- tests/Feature/LoggingTest.php
+++ tests/Feature/LoggingTest.php
@@ -20,6 +20,36 @@ class LoggingTest extends TestCase
         Log::warning("Hello Warning");
         Log::error("Hello Error");
         Log::critical("Hello Critical");
+        
+        assertTrue(true);
+    }
+    
+    function testContext()  {
+        Log::info('Hello Info', ["user" => "rena"]);
+        assertTrue(true);
+    }
+
+    function testWithContext()  { 
+        Log::withContext(["user" => "rena"]);
+        Log::info("Hello Info");
+        Log::error("Hello Warning");
+
+        assertTrue(true);
+    }
+
+    function testWithChannel() {
+        $slackLogger = Log::channel('slack');
+        $slackLogger->error('Hello Slack');
+
+        assertTrue(true); 
+    }
+
+    function testFileHandler() {
+        $fileLogger = Log::channel('file');
+        $fileLogger->info('Hello World');
+        $fileLogger->warning('Hello World');
+        $fileLogger->error('Hello World');
+        $fileLogger->critical('Hello World');
 
         assertTrue(true);
     }
